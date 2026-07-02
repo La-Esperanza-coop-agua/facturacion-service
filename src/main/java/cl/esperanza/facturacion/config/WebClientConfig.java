2d86bba63f761a88d20ae9d3af5ce37c316004e2
CyCklop <ignacioandresflores06@gmail.com>
2026-07-02 11:12:38 -0400

    modificado los beans en el config

--- src/main/java/cl/esperanza/facturacion/config/WebClientConfig.java
+++ src/main/java/cl/esperanza/facturacion/config/WebClientConfig.java
@@ -3,17 +3,18 @@ package cl.esperanza.facturacion.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
-    public WebClient sociosWebClient() {
-        return WebClient.builder().baseUrl("http://localhost:8082/api/v1/socios").build();
+    public WebClient sociosWebClient(@Value("${socios.service.url:http://localhost:8082/api/v1/socios}") String sociosServiceUrl) {
+        return WebClient.builder().baseUrl(sociosServiceUrl).build();
     }
 
     @Bean
-    public WebClient consumoWebClient(){
-        return WebClient.builder().baseUrl("http://localhost:8085/api/v1/lectura").build();
+    public WebClient consumoWebClient(@Value("${consumo.service.url:http://localhost:8085/api/v1/lectura}") String lecturaServiceUrl){
+        return WebClient.builder().baseUrl(lecturaServiceUrl).build();
     }
 }
